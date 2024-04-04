d25bd9c9e0e35ee990649a1345cb117830e2e1b3
Tyler <tyler@provenchermultimedia.com>
2024-04-04 18:42:51 -0400

    completed some of strings sectio

---
+++ Strings/designer_door_mat.py
@@ -0,0 +1,83 @@
+"""
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be X. ( is an odd natural number, and  is  times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+Input Format
+
+A single line containing the space separated values of  and .
+
+Constraints
+
+Output Format
+
+Output the design pattern.
+
+Sample Input
+
+9 27
+Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+"""
+n, m = map(int, input().split())
+
+special_design = ".|."
+holder = "-"
+top_lines = []
+
+# Top
+temp = special_design
+for i in range(n // 2):
+    line = temp
+    while len(line) < m:
+        line = holder + line + holder
+    top_lines.append(line)
+    temp = special_design + temp + special_design
+
+# Middle
+middle_line = "WELCOME"
+while len(middle_line) < m:
+    middle_line = holder + middle_line + holder
+
+# Print top lines
+for line in top_lines:
+    print(line)
+
+print(middle_line)
+
+# Bottom (reversing top_lines)
+for line in reversed(top_lines):
+    print(line)
